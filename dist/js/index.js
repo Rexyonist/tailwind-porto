110a3a2c21f0fdb9824fe4ac212cac03d16218d3
Bryant CH <bchihola@gmail.com>
2023-01-21 00:53:00 +0700

    update emailJS

---
+++ dist/js/index.js
@@ -0,0 +1,15 @@
+function sendMail(){
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    
+    emailjs
+    .send('service_qn9srvb', 'template_4kqsu5o', params)
+    .then(function(res){
+            console.log("success", res.status);
+            alert("Your Message Sent !")
+    })
+    .catch((err) => console.log(err));
+}
